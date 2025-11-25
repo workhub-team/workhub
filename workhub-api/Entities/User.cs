d56ff1dfe40cfc5115b53cd29f03965a2ea7cd48
dankrutov <daniborgesmarque@gmail.com>
2025-11-25 08:32:34 -0300

    fix: deletedAt e UpdatedAt agora são opcionais

--- workhub-api/Entities/User.cs
+++ workhub-api/Entities/User.cs
@@ -8,6 +8,6 @@ public class User
     public string Email { get; set; }
     public string Role { get; set; }
     public DateTime CreatedAt { get; set;}
-    public DateTime UpdatedAt { get; set;}
-    public DateTime DeletedAt { get; set;}
+    public DateTime? UpdatedAt { get; set;}
+    public DateTime? DeletedAt { get; set;}
 }
---
+++ workhub-api/Migrations/20251125113029_updated_user_deletedat.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace workhub_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class updated_user_deletedat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "UpdatedAt",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "DeletedAt",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime(6)",
+                oldNullable: true);
+        }
+    }
+}
