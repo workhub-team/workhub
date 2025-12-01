9ef725198e868c194cb728d32eaef09d1304ed3c
dankrutov <daniborgesmarque@gmail.com>
2025-12-01 16:41:15 -0300

    room table adjustments

---
+++ workhub-api/Migrations/20251201191434_room_added_name.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace workhub_api.Migrations
+{
+    /// <inheritdoc />
+    public partial class room_added_name : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Rooms",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Rooms");
+        }
+    }
+}
