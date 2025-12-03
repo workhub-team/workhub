1a01ff672e1e4ac960a40664f1b382aa84c84222
dankrutov <daniborgesmarque@gmail.com>
2025-12-03 17:00:10 -0300

    wip frontend

--- frontend/main.js
+++ frontend/main.js
@@ -1,5 +1,6 @@
 checkToken();
 
+// checagem de token
 function checkToken(){
     // search for token in local storage
     const token = localStorage.getItem("token");
@@ -24,6 +25,15 @@ function cleanUserData(){
     localStorage.removeItem("userRole")
     localStorage.removeItem("userId");
     localStorage.removeItem("userName");
+}
+
+function tokenValidate(token){
+    //check token expiration 
+    tokenPayload = JSON.parse(atob(token.split('.')[1]));
+    const currentTime = Math.floor(Date.now() / 1000);
+    const tokenexp = tokenPayload.exp;
+    console.log("Token expira em:", new Date(tokenexp * 1000));
+    return tokenexp > currentTime;
 }
 
 // login
@@ -62,11 +72,49 @@ async function tryLogin(email, senha){
     localStorage.setItem("userName", formatedResponse.user_name);
 }
 
-function tokenValidate(token){
-    //check token expiration 
-    tokenPayload = JSON.parse(atob(token.split('.')[1]));
-    const currentTime = Math.floor(Date.now() / 1000);
-    const tokenexp = tokenPayload.exp;
-    console.log("Token expira em:", new Date(tokenexp * 1000));
-    return tokenexp > currentTime;
+
+// cadastro
+document.getElementById('btn-criar').addEventListener('click', function(){
+    const nome = document.getElementById('nome').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const senha = document.getElementById('senha').value.trim();
+    if(!nome || !email || !senha){
+        alert('Por favor preencha todos os campos para criar a conta.');
+        return;
+    }
+    // simula criação e login
+    trySubscribe(nome, email, senha);
+});
+
+async function trySubscribe(nome, email, senha){
+    const formCriar = document.getElementById('form-criar');
+    const formEntrar = document.getElementById('form-entrar');
+    const areaAgendamento = document.getElementById('area-agendamento');
+    const aviso = document.getElementById('aviso');
+
+    const response = await fetch('http://localhost:5174/auth/register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'  
+        },
+        body: JSON.stringify({ 
+            username: nome,
+            password: senha,
+            email: email,
+        })
+    })
+
+    if (!response.ok) {
+        console.error(`Erro: ${response.status}`);
+
+        if (response.status === 409) {
+            alert('E-mail já cadastrado. Por favor, utilize outro e-mail.');
+            return;
+        }
+    }
+
+    const userid = await response.text();
+    console.log('usuário cadastrado realizado com sucesso:', userid)
+
+    alert('Conta criada com sucesso! Você está logado.');
 }
